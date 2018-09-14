80fd027234760bae266b10c4b3d9187e044da8fd
iangap <igaspar@novasolutionayatems.com>
2018-09-14 02:27:46 -0500

    Cambios para cubrir los requerimientos no funcionales de la practica.

--- loadData.py
+++ loadData.py
@@ -2,22 +2,30 @@ import csv
 import pymongo
 from pymongo import MongoClient
 
+#Datos de conexion a MongoDB
 client = MongoClient('localhost', 27017)
 db = client['newsdb']
 db.authenticate('cic', 'cic1234*')
 collection=db["news"]
 
+#lectura de archivo CSV
 with open('TodasLasNoticias.csv', 'r', encoding="utf8") as csvfile:
-    fileReader = csv.reader(csvfile, delimiter='|', quoting=csv.QUOTE_NONE)
+    fileReader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONE)
+#contadores de registros procesados
     item=0;
     item2=0;
+#For para iterar todos los registros del CSV
     for row in fileReader:
+#Se asume que un registro bien formado debe tener exactamente 5 elementos
        if len(row) == 5:
            data = {"date": row[0], "title": row[1], "url": row[2], "new": row[3], "categoria": row[4]}
            x = collection.insert_one(data)
            item2+=1
        else:
-           print("len: {}, row: {}".format(len(row), row))
+#Se considera que un registro del CSV con un numero de elementos distindo a 5 es erroneo y no se guarda en Base de Datos
+           #print("len: {}, row: {}".format(len(row), row))
            item+=1
+#Los registros OK_data son los registros guardados en BD
     print("OK_data:{}".format(item2))
+#Los registros bad_data son los que no cumplieron con los criterios de aceptacion y por lo tanto no se guardaron en BD.
     print("bad_data:{}".format(item))
--- modulo1.py
+++ modulo1.py
@@ -2,24 +2,32 @@ from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from bson.json_util import dumps
 
+#Datos de conexion a MongoDB
 client = MongoClient('localhost', 27017)
 db = client['newsdb']
 db.authenticate('cic', 'cic1234*')
 collection = db["news"]
 
+
 app = Flask(__name__)
 
+#ruta de test
 @app.route("/")
 def hello():
   return "hola mundo"
 
+#Endpoint noticias que tiene los parametros "categoria" y "palabra clave"
 @app.route('/noticias/<categoria>/<palabra_clave>',  methods=['GET'])
 def getData(categoria, palabra_clave):
+#Realiza una busqueda en MongoDb filtrando por el campo "categoria" y mediante el indice de busqueda de texto que trabaja sobre los campos "title" y "new"
     response = collection.find({"categoria": categoria, "$text": {"$search": palabra_clave}})
+#Formatea la respuesta a un tipo lista
     l = list(response)
+#Formatea la lista a tipo JSON
     dumps(l)
+#Retorna la respuesta como JSON
     return dumps(l)
 
-
+#inicia el servidor web
 if __name__ == "__main__":
   app.run(host='localhost', port=5000, debug=True)
